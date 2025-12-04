c62c25bade22d29f2d4eba6c0bc84415f40edd96
Michael Christensen <mlynnc@amazon.com>
2025-12-04 09:31:57 -0800

    Address pylint docstring warning

--- src/amazon-keyspaces-mcp-server/awslabs/amazon_keyspaces_mcp_server/__init__.py
+++ src/amazon-keyspaces-mcp-server/awslabs/amazon_keyspaces_mcp_server/__init__.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-###awslabs.amazon-keyspaces-mcp-server"""
+"""Amazon Keyspaces MCP Server package."""
 
 __version__ = '0.0.6'
